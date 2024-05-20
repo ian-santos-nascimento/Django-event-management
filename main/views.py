8166c6749e59c3463a86342f0b6833984efb1d33
Ian Santos <iansantosnascimento@gmail.com>
2024-05-20 11:04:13 -0300

    Git update requirements.txt

--- main/views.py
+++ main/views.py
@@ -45,9 +45,9 @@ def user_create(request):
             messages.success(request, 'Usuário salvo com sucesso!')
             return HttpResponseRedirect(reverse('user_list'))
         else:
-            return render(request, 'main/novoUsuario.html', {'form': form})
+            return render(request, 'usuarios/novoUsuario.html', {'form': form})
     form = RegistrationForm()
-    return render(request, 'main/novoUsuario.html', {'form': form})
+    return render(request, 'usuarios/novoUsuario.html', {'form': form})
 
 
 @login_required(login_url='login')
@@ -55,13 +55,13 @@ def user_list(request):
     if request.method == 'POST' and 'editUser' in request.POST:
         user = User.objects.get(pk=request.POST['userId'])
         form = EditUserForm(instance=user)
-        return render(request, 'main/editarUsuario.html', {'form': form})
+        return render(request, 'usuarios/editarUsuario.html', {'form': form})
     elif request.method == 'POST':
         return generate_registration_form(request)
     if request.method == 'GET':
         messages = get_messages(request)
         usuarios = User.objects.exclude(username='admin')
-        return render(request, 'main/usuarios.html', {'usuarios': usuarios, 'messages': messages})
+        return render(request, 'usuarios/usuarios.html', {'usuarios': usuarios, 'messages': messages})
 
 
 @login_required(redirect_field_name='login')
@@ -83,7 +83,7 @@ def client_create(request):
     else:
         form = CreateClientForm()
 
-    return render(request, 'main/novoCliente.html', {'form': form})
+    return render(request, 'clientes/novoCliente.html', {'form': form})
 
 
 @login_required(login_url='login')
@@ -93,12 +93,12 @@ def client_list(request):
             client_id = request.POST.get('clienteId')
             client = Cliente.objects.get(pk=client_id)
             form = CreateClientForm(instance=client)
-            return render(request, 'main/novoCliente.html', {'form': form, 'client_id': client_id})
+            return render(request, 'clientes/novoCliente.html', {'form': form, 'client_id': client_id})
         else:
             return generate_cliente_registration_form(request)
     elif request.method == 'GET':
         clientes = Cliente.objects.all()
-        return render(request, 'main/clientes.html', {'clientes': clientes})
+        return render(request, 'clientes/clientes.html', {'clientes': clientes})
 
 
 @login_required(login_url='login')
@@ -108,7 +108,7 @@ def comida_list(request):
         if is_file_upload(request):
             return handle_file_upload(request)
         else:
-            return handle_comida_edit(request)
+            return handle_comida(request)
     return render(request, 'comidas/comidas.html', {'comidas': comidas})
 
 
@@ -132,12 +132,12 @@ def comida_create(request):
 #
 def generate_registration_form(request):
     form = RegistrationForm()
-    return render(request, 'main/novoUsuario.html', {'form': form})
+    return render(request, 'usuarios/novoUsuario.html', {'form': form})
 
 
 def generate_cliente_registration_form(request):
     form = CreateClientForm()
-    return render(request, 'main/novoCliente.html', {'form': form})
+    return render(request, 'clientes/novoCliente.html', {'form': form})
 
 
 def get_paginated_comidas(request):
@@ -171,7 +171,7 @@ def handle_file_upload(request):
     return HttpResponseRedirect(reverse('comida_list'))
 
 
-def handle_comida_edit(request):
+def handle_comida(request):
     if 'editComida' in request.POST:
         comida_id = request.POST.get('comidaId')
         comida = Comida.objects.get(pk=comida_id)
@@ -183,7 +183,7 @@ def handle_comida_edit(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Comida editada com sucesso!')
-
         return HttpResponseRedirect(reverse('comida_list'))
+
     form = CreateComidaForm()
     return render(request, 'comidas/novaComida.html', {'form': form})
