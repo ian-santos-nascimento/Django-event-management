99081ba008a88aa856ef629c986f245fbc209809
Ian Santos <iansantosnascimento@gmail.com>
2024-07-27 12:32:20 -0300

    fix: ComidaOrçamento sendo deletada para salvar novas linhas

--- main/models.py
+++ main/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-
-from .service import incluiAgravoRegiao
 from .utils import listSelect
 
 
--- main/views.py
+++ main/views.py
@@ -1,22 +1,22 @@
 from django.contrib import messages
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate
+from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.messages import get_messages
 from django.http import HttpResponseRedirect
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.urls import reverse
-from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework.decorators import api_view, permission_classes
-from django.contrib.auth import get_user_model, login, logout
-from rest_framework.authentication import SessionAuthentication
-from rest_framework.views import APIView
-from rest_framework.response import Response
-
-from .models import Logistica, Orcamento, LogisticaOrcamento, Evento, Cliente, Comida
-from .serializers import UserLoginSerializer, UserSerializer
 from rest_framework import permissions, status
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from .forms import *
+from .models import Logistica, Orcamento, Evento, Cliente, Comida, ComidaOrcamento
+from .serializers import UserLoginSerializer
 
 
 @csrf_exempt
@@ -38,21 +38,18 @@ def login_view(request):
 def saveOrcamento(request):
     evento_id = request.data.get('evento')
     cliente_id = request.data.get('cliente')
-    logisticas = request.data.get('logisticas')
-    nome_evento = request.data.get('nome')
-    comidas = request.data.get('comidas')
-    observacoes_evento = request.data.get('observacoes')
-    valor_total_comidas = request.data.get('valor_total_comidas')
-    desconto_total_comidas = request.data.get('desconto_total_comidas')
-    valor_total_logisticas = request.data.get('valor_total_logisticas')
-    desconto_total_logisticas = request.data.get('desconto_total_logisticas')
-
     try:
         evento = Evento.objects.get(pk=evento_id)
         cliente = Cliente.objects.get(pk=cliente_id)
     except (Evento.DoesNotExist, Cliente.DoesNotExist) as e:
         return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
+    nome_evento = request.data.get('nome')
+    observacoes_evento = request.data.get('observacoes')
+    valor_total_comidas = request.data.get('valor_total_comidas')
+    desconto_total_comidas = request.data.get('desconto_total_comidas')
+    valor_total_logisticas = request.data.get('valor_total_logisticas')
+    desconto_total_logisticas = request.data.get('desconto_total_logisticas')
     orcamento = Orcamento(
         evento=evento,
         cliente=cliente,
@@ -65,28 +62,9 @@ def saveOrcamento(request):
         valor_total=valor_total_logisticas + valor_total_comidas,
     )
     orcamento.save()
-
-    for logistica in logisticas:
-        try:
-            logistica_bd = Logistica.objects.get(pk=logistica.get('id'))
-            quantidade = logistica.get('quantidade', 1)
-            valor = logistica_bd.valor
-
-            orcamento.logisticas.add(logistica_bd, through_defaults={'quantidade': quantidade, 'valor': valor})
-        except Logistica.DoesNotExist:
-            continue
-    for comida in comidas:
-        try:
-            comida_bd = Comida.objects.get(pk=comida.get('id'))
-            quantidade = comida.get('quantidade', 1)
-            valor = comida_bd.valor
-            orcamento.comidas.add(comida_bd, through_defaults={'quantidade': quantidade, 'valor': valor})
-        except Comida.DoesNotExist:
-            continue
-        except Exception as e:
-            print('Exception while saving Orcamento:' + str(e))
-    return Response({'message': 'Orcamento criado com sucesso', 'id': orcamento.id_orcamento},
-                    status=status.HTTP_200_OK)
+    create_comida_for_orcamento(request, orcamento)
+    create_logistica_for_orcamento(request, orcamento)
+    return Response('Orçamento salvo com sucesso!', status=status.HTTP_200_OK)
 
 
 class UserLogin(APIView):
@@ -139,3 +117,38 @@ def user_list(request):
 def generate_registration_form(request):
     form = RegistrationForm()
     return render(request, 'usuarios/novoUsuario.html', {'form': form})
+
+
+def create_comida_for_orcamento(request, orcamento):
+    comidas = request.data.get('comidas')
+    for comida in comidas:
+        try:
+            comida_bd = Comida.objects.get(pk=comida.get('id'))
+            quantidade = comida.get('quantidade', 1)
+            valor = comida_bd.valor
+            ComidaOrcamento.objects.update_or_create(
+                orcamento=orcamento,
+                comida=comida_bd,
+                defaults={'quantidade': quantidade, 'valor': valor}
+            )
+        except Comida.DoesNotExist:
+            continue
+        except Exception as e:
+            print('Exception ao salvar Comida para Orcamento:' + str(e))
+            return Response('Houve um erro ao salvar a comida', status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+def create_logistica_for_orcamento(request, orcamento):
+    logisticas = request.data.get('logisticas')
+    for logistica in logisticas:
+        try:
+            logistica_bd = Logistica.objects.get(pk=logistica.get('id'))
+            quantidade = logistica.get('quantidade', 1)
+            valor = logistica_bd.valor
+
+            orcamento.logisticas.add(logistica_bd, through_defaults={'quantidade': quantidade, 'valor': valor})
+        except Logistica.DoesNotExist:
+            continue
+        except Exception as e:
+            print('Exception ao salvar Logistica para Orcamento:' + str(e))
+            return Response('Houve um erro ao salvar a logistica', status=status.HTTP_500_INTERNAL_SERVER_ERROR)
